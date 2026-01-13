ded2aa76215b24a7dd0eb104dedf75ef077f06eb
Alan Ezequiel Roth <rothalan83@gmail.com>
2026-01-13 15:13:45 -0300

    Add files via upload

--- MaquinaDePatada.ino
+++ MaquinaDePatada.ino
@@ -1,8 +1,12 @@
 #include <Arduino.h>
+#include <LiquidCrystal_I2C.h>
 
 // --- Configuración de Hardware ---
-const int PIN_S1 = 26; // Sensor de inicio (ranura 1)
-const int PIN_S2 = 25; // Sensor de fin (ranura 2)
+#define PIN_S1 26 // Sensor de inicio (ranura 1)
+#define PIN_S2 25 // Sensor de fin (ranura 2)
+#define BTNConfig 4 //boton para entrar en configuracion 
+#define BTNUp 12 //boton para aumentar o subir
+#define BTNDown 13 // boton para disminuir o bajar
 
 // --- Parámetros de la Máquina ---
 const float DISTANCIA_M = 0.12;       // 12 cm convertidos a metros
@@ -14,6 +18,13 @@ volatile unsigned long t1 = 0;
 volatile unsigned long t2 = 0;
 volatile bool calculando = false;
 volatile bool error_timeout = false;
+
+// --- Variables de LCD ---
+int lcdColumns = 16;
+int lcdRows = 2;
+LiquidCrystal_I2C lcd(0x27, lcdColumns, lcdRows);
+
+float Podio[3];
 
 // --- ISR: Interrupción Sensor 1 ---
 void IRAM_ATTR isr_inicio() {
@@ -33,11 +44,82 @@ void IRAM_ATTR isr_fin() {
   }
 }
 
+bool leerBotonConDebounce(int pin, int delayMs = 50) {
+    if (digitalRead(pin) == LOW) {
+        delay(delayMs);
+        if (digitalRead(pin) == LOW) {
+            while (digitalRead(pin) == LOW) delay(10);
+            delay(50); return true;
+        }
+    }
+    return false;
+}
+
+void Configurar(){
+
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("VERSION 1");
+  lcd.setCursor(0,1);
+  lcd.print("01/02/26");
+  delay(500);
+
+    // Esperar a que se suelte DATO3 si está presionado
+  while (digitalRead(BTNConfig) == LOW) {
+      delay(20);
+  }
+
+  delay(100);
+
+      // Esperar a que se presione DATO3
+  while (digitalRead(BTNConfig) == HIGH) {
+      delay(20);
+  }
+  // Esperar a que se suelte
+  while (digitalRead(BTNConfig) == LOW) {
+      delay(20);
+  }
+  delay(100);
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Ajustar Dificultad");
+  delay(500);
+
+  while (digitalRead(BTNConfig) == HIGH) {
+      lcd.setCursor(0,1);
+      lcd.print("   ");  // Limpiar
+      lcd.setCursor(0,1);
+      lcd.print(factorDificultad);
+      
+      if (leerBotonConDebounce(BTNUp)) {
+          factorDificultad += 0.1;
+      }
+      if (leerBotonConDebounce(BTNDown)) {
+          factorDificultad -= 0.1;
+      }
+      delay(10);
+  }
+
+  while(digitalRead(BTNConfig) == LOW) {
+    delay(20);
+  }
+
+  delay(100);
+
+  return;
+}
+
 void setup() {
   Serial.begin(115200);
-  
+
+  lcd.init(); lcd.backlight(); //inicializo lcd
+
   pinMode(PIN_S1, INPUT_PULLUP);
   pinMode(PIN_S2, INPUT_PULLUP);
+  pinMode(BTNConfig, INPUT_PULLUP);
+  pinMode(BTNUp, INPUT_PULLUP);
+  pinMode(BTNDown, INPUT_PULLUP);
 
   // Interrupción en FALLING (cuando la aleta entra en la ranura y corta la luz)
   attachInterrupt(digitalPinToInterrupt(PIN_S1), isr_inicio, FALLING);
@@ -48,6 +130,7 @@ void setup() {
 }
 
 void loop() {
+  
   // 1. Verificar si se completó el recorrido
   if (calculando && t2 > 0) {
     unsigned long duracion_us = t2 - t1;
@@ -60,10 +143,31 @@ void loop() {
     // Una patada rápida puede ir a 10-15 m/s
     float puntaje = (velocidad_ms * 100) / factorDificultad;
 
+    if(puntaje > 999) puntaje = 999;
+
+    for (int i = 0; i < 3; i++) {
+      if (puntaje > Podio[i]) {
+        // 1. Desplazar los puntajes hacia abajo desde el final hasta la posición i
+        for (int j = 2; j > i; j--) {
+          Podio[j] = Podio[j - 1];
+        }
+        
+        // 2. Insertar el nuevo puntaje en la posición i
+        Podio[i] = (int)puntaje;
+        
+        break; 
+      }
+    }
+
     Serial.println("\n--- RESULTADO ---");
     Serial.printf("Tiempo: %.4f seg\n", segundos);
     Serial.printf("Velocidad: %.2f m/s\n", velocidad_ms);
     Serial.printf("PUNTAJE: %d\n", (int)puntaje);
+    Serial.println("--- PODIO ---");
+    for(int i = 0; i < 3; i++) {
+        // IMPORTANTE: Usamos %.0f porque Podio es un array de floats.
+        Serial.printf("Puesto %d: %.0f\n", i + 1, Podio[i]);
+    }
     Serial.println("-----------------");
 
     delay(3000); // Pausa para mostrar el puntaje
@@ -82,4 +186,6 @@ void loop() {
       calculando = false;
     }
   }
+
+  if(digitalRead(BTNConfig) == LOW) Configurar();
 }
